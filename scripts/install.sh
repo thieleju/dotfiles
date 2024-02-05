5eee345f35e6d582c62c22ceb89f7eef4e3ed449
thieleju <thiele.juli@gmail.com>
2024-02-05 01:14:03 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,10 +15,8 @@ execute_and_log() {
   log_message="Executing: $command"
   echo "$log_message"
   echo "$log_message" >> "$LOG_FILE"
-  
   # Log the message to a file
   eval "$command" >> "$LOG_FILE" 2>&1
-  
   if [ $? -eq 0 ]; then
     echo "Command successfully executed." >> "$LOG_FILE"
   else
@@ -37,15 +35,24 @@ command_exists() {
 install_tool() {
   tool_name=$1
   install_command=$2
-
-  # Check if the tool is already installed
   if command_exists "$tool_name"; then
     echo "$tool_name is already installed."
   else
-    # Install the tool
     execute_and_log "$install_command"
     echo "$tool_name has been successfully installed."
   fi
+}
+
+# Function to remove a directory if it exists and clone a Git repository
+remove_and_clone() {
+  repo_url=$1
+  target_dir=$2
+  # Remove existing directory if it exists
+  if [ -d "$target_dir" ]; then
+    execute_and_log "rm -rf $target_dir"
+  fi
+  # Clone the Git repository
+  execute_and_log "git clone $repo_url $target_dir"
 }
 
 # Install debian packages
@@ -60,7 +67,16 @@ install_tool "zsh" "sudo apt-get install -y zsh"
 
 # Copy the .gitconfig file
 echo -e "\nSetting up .gitconfig and installing lazygit..."
-execute_and_log "cp -f '$DOTFILES_DIR/git/.gitconfig' ~/.gitconfig"
+GITCONFIG_PATH="$HOME/.gitconfig"
+GITCONFIG_BACKUP="$HOME/.gitconfig_backup"
+
+# Backup existing .gitconfig, if any
+if [ -f "$GITCONFIG_PATH" ]; then
+  execute_and_log "mv $GITCONFIG_PATH $GITCONFIG_BACKUP"
+fi
+
+execute_and_log "cp -f '$DOTFILES_DIR/git/.gitconfig' $GITCONFIG_PATH"
+
 # Install LazyGit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 execute_and_log "curl -Lo lazygit.tar.gz 'https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz'"
@@ -69,46 +85,72 @@ execute_and_log "sudo install lazygit /usr/local/bin"
 
 # Install oh-my-zsh and plugins
 echo -e "\nInstalling Zsh and plugins..."
-execute_and_log "git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh"
+OH_MY_ZSH_DIR=~/.oh-my-zsh
+
+# Remove existing directory and clone oh-my-zsh
+remove_and_clone "https://github.com/ohmyzsh/ohmyzsh.git" "$OH_MY_ZSH_DIR"
+
 # Copy the .zshrc and .p10k.zsh files
 execute_and_log "cp -f '$DOTFILES_DIR/zsh/.zshrc' ~/.zshrc"
 execute_and_log "cp -f '$DOTFILES_DIR/zsh/.p10k.zsh' ~/.p10k.zsh"
 
 # Install powerlevel10k theme
-execute_and_log "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+POWERLEVEL10K_DIR=${ZSH_CUSTOM:-$OH_MY_ZSH_DIR/custom}/themes/powerlevel10k
+
+# Remove existing directory and clone powerlevel10k
+remove_and_clone "https://github.com/romkatv/powerlevel10k.git" "$POWERLEVEL10K_DIR"
+
 # Install zsh-autosuggestions, zsh-syntax-highlighting, and zsh-nvm plugins
-execute_and_log "git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-execute_and_log "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
-execute_and_log "git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm"
+ZSH_AUTOSUGGESTIONS_DIR=$OH_MY_ZSH_DIR/custom/plugins/zsh-autosuggestions
+ZSH_SYNTAX_HIGHLIGHTING_DIR=$OH_MY_ZSH_DIR/custom/plugins/zsh-syntax-highlighting
+ZSH_NVM_DIR=$OH_MY_ZSH_DIR/custom/plugins/zsh-nvm
+
+# Remove existing directories and clone plugins
+remove_and_clone "https://github.com/zsh-users/zsh-autosuggestions" "$ZSH_AUTOSUGGESTIONS_DIR"
+remove_and_clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$ZSH_SYNTAX_HIGHLIGHTING_DIR"
+remove_and_clone "https://github.com/lukechilds/zsh-nvm" "$ZSH_NVM_DIR"
 
 # Install Tmux Plugin Manager
 echo -e "\nInstalling Tmux Plugin Manager..."
-execute_and_log "mkdir -p ~/.config/tmux"
-execute_and_log "cp -f '$DOTFILES_DIR/tmux/tmux.conf' ~/.config/tmux/tmux.conf"
-execute_and_log "git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm"
-execute_and_log "~/.tmux/plugins/tpm/bin/install_plugins"
+TMUX_CONFIG_DIR=~/.config/tmux
+TMUX_PLUGIN_MANAGER_DIR=~/.tmux/plugins/tpm
+
+# Remove existing directories and clone Tmux Plugin Manager
+remove_and_clone "https://github.com/tmux-plugins/tpm" "$TMUX_PLUGIN_MANAGER_DIR"
+
+execute_and_log "mkdir -p $TMUX_CONFIG_DIR"
+execute_and_log "cp -f '$DOTFILES_DIR/tmux/tmux.conf' $TMUX_CONFIG_DIR"
+execute_and_log "$TMUX_PLUGIN_MANAGER_DIR/bin/install_plugins"
 
 # Install and configure Neovim
 echo -e "\nInstalling and configuring Neovim..."
+NEOVIM_CONFIG_DIR=~/.config/nvim
+
+# Remove existing directory and clone Neovim configuration
+remove_and_clone "https://github.com/thieleju/neovim.git" "$DOTFILES_DIR/nvim"
+
 execute_and_log "curl -Lo nvim.appimage https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
 execute_and_log "chmod +x nvim.appimage"
 execute_and_log "./nvim.appimage --appimage-extract"
 execute_and_log "sudo ln -s /squashfs-root/AppRun /usr/bin/nvim"
 execute_and_log "sudo mv squashfs-root /"
-# Set up Neovim configuration
-execute_and_log "git clone https://github.com/thieleju/neovim.git '$DOTFILES_DIR/nvim'"
-execute_and_log "mkdir -p ~/.config/nvim/"
-execute_and_log "mv '$DOTFILES_DIR/nvim/'* ~/.config/nvim/"
+execute_and_log "mkdir -p $NEOVIM_CONFIG_DIR"
+execute_and_log "mv $DOTFILES_DIR/nvim/* $NEOVIM_CONFIG_DIR"
 
 # Print Neovim version
 execute_and_log "nvim --version"
 
 # Cleanup artifacts
 echo -e "\nCleaning up artifacts..."
-execute_and_log "rm -f nvim.appimage"  # Remove the Neovim AppImage
+execute_and_log "rm -f nvim.appimage"   # Remove the Neovim AppImage
 execute_and_log "rm -rf squashfs-root"  # Remove the extracted squashfs-root directory
 execute_and_log "rm -f lazygit.tar.gz"  # Remove the LazyGit tarball
 execute_and_log "rm -f lazygit"         # Remove the extracted LazyGit binary (if any)
+
+# Restore original .gitconfig if backup exists
+if [ -f "$GITCONFIG_BACKUP" ]; then
+  execute_and_log "mv $GITCONFIG_BACKUP $GITCONFIG_PATH"
+fi
 
 # Set Zsh as the default shell
 if [ "$SHELL" != "$(which zsh)" ]; then
